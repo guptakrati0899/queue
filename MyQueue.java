613030c1f3e006e5ce3ceaaf0b57bd96d80b3763
KratiGupta <kratigupta8545@gmail.com>
2021-08-07 16:29:14 +0530

    Added welcome msg successfully

---
+++ MyQueue.java
@@ -0,0 +1,16 @@
+package queue;
+
+import linkedlist.INode;
+import stack.MyLinkedList;
+
+public class MyQueue {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Queue Master Branch");
+	}
+	
+	
+
+
+}
